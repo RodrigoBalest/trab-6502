c99cd11ec72ddf03e674d78e24fd6b7de155c6da
RodrigoBalest <r.balest@gmail.com>
2019-11-05 12:47:06 -0300

    WIP movimentação

--- code.asm
+++ code.asm
@@ -1,7 +1,9 @@
 define sysRandom    $fe
 define sysLastKey   $ff
+define position     $00
 
 LDX #$10
+STX position
 
 loop:
   JSR drawShip
@@ -10,6 +12,7 @@ loop:
   
 drawShip:
   LDA #$0E
+  LDX position
   STA $05C0, X
   DEX
   STA $05E0, X
@@ -21,6 +24,19 @@ drawShip:
   RTS
 
 readInput:
+  LDX position
   LDY sysLastKey
+;  CPY #$64
+;  BEQ goRight
+;  CPY #$61
+;  BEQ goLeft
+;  CPY #$77
+;  BEQ shoot
   RTS
+
+;goRight:
+;  CPX $#1E ;máximo à direita
+   
+;goLeft:
+;  CPX $#01 ;máximo à esquerda
   
