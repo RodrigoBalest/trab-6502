92d8f62e5e40e53f608b988b1171477e475741c8
RodrigoBalest <r.balest@gmail.com>
2019-11-06 23:01:42 -0300

    It's shooting!

--- code.asm
+++ code.asm
@@ -2,18 +2,24 @@ define sysRandom    $fe
 define sysLastKey   $ff
 define position     $00
 define shots        $10 ;primeira célula que armazenará os tiros
+define temp         $F0
 
 LDX #$10
 STX position
 
 loop:
-  LDA #$00
-  JSR drawShip  ;pinta nave de preto
   JSR readInput ;atualiza posição
   LDA #$0E
   JSR drawShip  ;pinta nave de azul
+  LDA #$01
+  JSR drawShots ;pinta tiros de branco
   JSR resetInput
   JSR delay
+  LDA #$00
+  JSR drawShip  ;pinta nave de preto
+  LDA #$00
+  JSR drawShots ;pinta tiros de preto
+  ;JSR updateShots ;movimenta os tiros
   JMP loop
   
 drawShip:
@@ -31,9 +37,9 @@ drawShip:
 readInput:
   LDX position
   LDY sysLastKey
-  CPY #$44
+  CPY #$64
   BEQ goRight
-  CPY #$41
+  CPY #$61
   BEQ goLeft
   CPY #$77
   BEQ shoot
@@ -58,7 +64,42 @@ goLeft:
   RTS
 
 shoot:
-  LDX #$00
+  LDX #$01
+  checkIndexHasShot:
+  LDA shots,X
+  CMP #$00 ; verifica se há um valor armazenado
+  BEQ addShot
+  INX
+  INX
+  CPX #$06 ; max 3 tiros
+  BMI checkIndexHasShot
+  RTS
+
+addShot:
+  LDA #$05
+  STA shots,X
+  DEX
+  LDA position
+  ADC #$BF
+  STA shots,X
+  RTS
+
+drawShots:
+  STA temp ;armazena a cor em uma variável temporária
+  LDX #$01
+  drawNextShot:
+  LDA shots,X
+  CMP #$00 ;verifica se há um valor armazenado
+  BEQ skipDrawShot ;não há tiro para desenhar
+  LDA temp ;carrega a cor
+  DEX
+  STA (shots,X) ;pinta a posição do tiro
+  INX
+  skipDrawShot:
+  INX
+  INX
+  CPX #$06 ; max 3 tiros
+  BMI drawNextShot
   RTS
 
 delay:
